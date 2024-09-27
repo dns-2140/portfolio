9837f3983d12960d7d18983fa71da580015a5242
dnsdev2140 <davidns2140@gmail.com>
2024-09-27 17:07:01 +0700

    improve form validation

--- index.js
+++ index.js
@@ -21,33 +21,58 @@ feForm.addEventListener('submit', (e) => {
   const companyVal = document.querySelector('#company').value;
   const startVal = document.querySelector('#start').value;
   const endVal = document.querySelector('#end').value;
-  if (valFe) {
-    feList.insertAdjacentHTML('beforeend', `<li>${valFe}</li>`);
-  }
 
-  if (valBe) {
-    beList.insertAdjacentHTML('beforeend', `<li>${valBe}</li>`);
+  if (!valFe && !valBe && !companyVal && !startVal && !endVal) {
+    alert('kamu tidak memasukkan data sama sekali!');
+  } else {
+    if (valFe) {
+      feList.insertAdjacentHTML('beforeend', `<li>${valFe}</li>`);
+    }
+
+    if (valBe) {
+      beList.insertAdjacentHTML('beforeend', `<li>${valBe}</li>`);
+    }
+
+    if (startVal && endVal && companyVal) {
+      jobList.insertAdjacentHTML(
+        'beforeend',
+        `<article>
+              <figure>
+                <div>
+                  <img
+                    src="https://images.pexels.com/photos/716276/pexels-photo-716276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2""
+                    width="100%"
+                  />
+                  <figcaption>${companyVal}</figcaption>
+                </div>
+              </figure>
+              <h3>${companyVal}</h3>
+              <div>${startVal}-${endVal}</div>
+              <p></p>
+            </article>`
+      );
+    }
+
+    if (startVal || endVal || companyVal) {
+      if (!startVal && !endVal) {
+        alert('kamu belum memasukkan tanggal masuk dan keluar perusahaan!');
+      } else if (!startVal && !companyVal) {
+        alert(
+          'kamu belum memasukkan nama perusahaan dan tanggal masuk perusahaan'
+        );
+      } else if (!endVal && !companyVal) {
+        alert(
+          'kamu belum memasukkan nama perusahaan dan tanggal keluar perusahaan!'
+        );
+      } else if (!companyVal) {
+        alert('kamu belum memasukkan nama perusahaan!');
+      } else if (!endVal) {
+        alert('kamu belum memasukkan tanggal keluar perusahaan!');
+      } else if (!startVal) {
+        alert('kamu belum memasukkan tanggal masuk perusahaan!');
+      }
+    }
   }
-  if (startVal && endVal && companyVal) {
-    jobList.insertAdjacentHTML(
-      'beforeend',
-      `<article>
-            <figure>
-              <div>
-                <img
-                  src="https://images.pexels.com/photos/716276/pexels-photo-716276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2""
-                  width="100%"
-                />
-                <figcaption>${companyVal}</figcaption>
-              </div>
-            </figure>
-            <h3>${companyVal}</h3>
-            <div>${startVal}-${endVal}</div>
-            <p></p>
-          </article>`
-    );
-  }
-  console.log(startVal, endVal, companyVal);
 });
 
 const admin = document.getElementById('admin');
